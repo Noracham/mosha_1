d399d8ffa0e2e4b74f0e87efb653d2b052b7aa33
Nora <nsorym04@gmail.com>
2023-02-16 16:49:00 +0900

    基本ファイルとスタイルの読み込みフック

---
+++ wp-content/themes/mosha_1/functions.php
@@ -0,0 +1,7 @@
+<?php
+function load_my_style()
+{
+    wp_enqueue_style('style', get_template_directory_uri() . "css/style.css");
+    wp_enqueue_style('normalize', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css', array('style'));
+};
+add_action('wp_enqueue_scripts', 'load_my_style');
