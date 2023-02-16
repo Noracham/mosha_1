c556c5951119cf5e2b14a2e38637580906d1c248
Nora <nsorym04@gmail.com>
2023-02-16 17:29:32 +0900

    header完成

---
+++ wp-content/themes/mosha_1/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="header">
+        <nav>
+            <div class="logo"><img src="<?php echo esc_url(get_theme_file_uri('img/logo.svg')) ?>" alt="logo"></div>
+            <div class="nav-link">
+                <a href="#about">About</a>
+                <a href="#Bicycle">Bicycle</a>
+            </div>
+        </nav>
+    </header>
