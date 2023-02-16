748a48aba3b329afc1a09f847d4ae982fce062e5
Nora <nsorym04@gmail.com>
2023-02-16 22:53:04 +0900

    2/16 finish

--- wp-content/themes/mosha_1/header.php
+++ wp-content/themes/mosha_1/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo esc_url(get_theme_file_uri('img/favicon.ico')) ?>">
     <title><?php echo bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
