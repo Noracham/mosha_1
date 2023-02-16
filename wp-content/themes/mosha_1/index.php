13599628f5feee16fec4eeacd899c1714075886a
Nora <nsorym04@gmail.com>
2023-02-16 18:33:05 +0900

    aboutセクション完成

--- wp-content/themes/mosha_1/index.php
+++ wp-content/themes/mosha_1/index.php
@@ -1,4 +1,15 @@
 <?php get_header(); ?>
 <div class="hero"><img src="<?php echo esc_url(get_theme_file_uri('img/mainvisual.jpg')) ?>" alt="hero"></div>
-
+<section class="about wrapper">
+    <h1 class="sub-title">About</h1>
+    <div class="profile">
+        <div class="profile-img"><img src="<?php echo esc_url(get_theme_file_uri('img/about.jpg')) ?>" alt="me"></div>
+        <div class="profile-txt">
+            <h2>KAKERU MIYAICHI</h2>
+            <p>テキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキスト</p>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
